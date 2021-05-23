e6be8f23b5afc31b33a54e97f96229eef294c83c
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-05-23 00:26:30 -0500

    created manager js file

---
+++ index.js
@@ -0,0 +1,32 @@
+const inquirer = require("inquirer");
+const { type } = require("os");
+const Employee = require("./lib/Employee");
+const initializeQuestions = () => {
+  inquirer
+    .prompt([
+      {
+        type: "text",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "number",
+        name: "id",
+        message: "What is your id?",
+        validate: (number) => {
+          if (typeof number === "number") {
+            return number;
+          }
+        },
+      },
+      {
+        type: "text",
+        name: "email",
+        message: "What is your email address?",
+      },
+    ])
+    .then((responses) => console.log(responses));
+  console.log(new Employee(response.name, response.id, respone.email));
+};
+
+initializeQuestions();
