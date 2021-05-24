b8ca702831b69d2bce09bfd88ae8e9a6904bb3a0
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-05-24 01:02:38 -0500

    created functions for mangager, engineer and intern with questions

--- index.js
+++ index.js
@@ -1,32 +1,107 @@
 const inquirer = require("inquirer");
 const { type } = require("os");
 const Employee = require("./lib/Employee");
+
+const baseQuestions = [
+  {
+    type: "text",
+    name: "name",
+    message: "What is the employees name?",
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "What is their id?",
+  },
+  {
+    type: "text",
+    name: "email",
+    message: "What is their email address?",
+  },
+];
+
 const initializeQuestions = () => {
   inquirer
     .prompt([
       {
-        type: "text",
-        name: "name",
-        message: "What is your name?",
-      },
-      {
-        type: "number",
-        name: "id",
-        message: "What is your id?",
-        validate: (number) => {
-          if (typeof number === "number") {
-            return number;
-          }
-        },
-      },
-      {
-        type: "text",
-        name: "email",
-        message: "What is your email address?",
+        type: "list",
+        name: "employeeType",
+        message: "Choose an employee type",
+        choices: ["manager", "engineer", "intern"],
       },
     ])
-    .then((responses) => console.log(responses));
-  console.log(new Employee(response.name, response.id, respone.email));
+    .then((responses) => {
+      switch (responses.employeeType) {
+        case "manager":
+          createManager();
+          break;
+        case "engineer":
+          createEngineer();
+          break;
+        case "intern":
+          createIntern();
+          break;
+      }
+    });
 };
-
+const createManager = () => {
+  inquirer
+    .prompt([
+      ...baseQuestions,
+      {
+        type: "number",
+        name: "officeNumber",
+        message: "Enter their office number",
+      },
+    ])
+    .then((responses) => {
+      console.log(responses);
+      addEmployee();
+    });
+};
+const createEngineer = () => {
+  inquirer
+    .prompt([
+      ...baseQuestions,
+      {
+        type: "input",
+        name: "github",
+        message: "Enter their github username",
+      },
+    ])
+    .then((responses) => {
+      console.log(responses);
+      addEmployee();
+    });
+};
+const createIntern = () => {
+  inquirer
+    .prompt([
+      ...baseQuestions,
+      {
+        type: "input",
+        name: "school",
+        message: "Enter what school they attended",
+      },
+    ])
+    .then((responses) => {
+      console.log(responses);
+      addEmployee();
+    });
+};
+const addEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "addEmployee",
+        message: "Would you like to add another employee?",
+      },
+    ])
+    .then((responses) => {
+      if (responses.addEmployee) {
+        initializeQuestions();
+      }
+    });
+};
 initializeQuestions();
