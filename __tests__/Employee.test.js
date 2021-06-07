bc1d6ce805af6d0f65f328051e8937f597a5970d
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-06-06 20:20:52 -0500

    created all test files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const Employee = require("../lib/Employee");
+
+// jest.mock('../lib/Employee')
+
+test("Get employee information based off inquirer prompts", () => {
+  const employee = new Employee("Lexie", 4325, "lex@gmail.com", "Employee");
+
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.getRole()).toBe("Employee");
+});
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,13 @@
+const Manager = require("../lib/Manager");
+
+// jest.mock('../lib/manager')
+
+test("Get manager information based off inquirer prompts", () => {
+  const manager = new Manager("Lexie", 4325, "lex@gmail.com", 3631977);
+
+  expect(manager.name).toEqual(expect.any(String));
+  expect(manager.id).toEqual(expect.any(Number));
+  expect(manager.email).toEqual(expect.any(String));
+  expect(manager.officeNumber).toEqual(expect.any(Number));
+  expect(manager.getRole()).toBe("Manager");
+});
