168b1794dde3ca29b5ef30f6f100aa1288c243d8
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-07-09 18:31:15 -0500

    completed tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,63 @@
+const { TestScheduler } = require("jest");
 const Employee = require("../lib/Employee");
 
 // jest.mock('../lib/Employee')
 
-test("Get employee information based off inquirer prompts", () => {
-  const employee = new Employee("Lexie", 4325, "lex@gmail.com", "Employee");
+// test("Get employee information based off inquirer prompts", () => {
+//   const employee = new Employee("Leti", 4325, "let@gmail.com", "Employee");
 
-  expect(employee.name).toEqual(expect.any(String));
-  expect(employee.id).toEqual(expect.any(Number));
-  expect(employee.email).toEqual(expect.any(String));
-  expect(employee.getRole()).toBe("Employee");
+//   expect(employee.name).toEqual(expect.any(String));
+//   expect(employee.id).toEqual(expect.any(Number));
+//   expect(employee.email).toEqual(expect.any(String));
+//   expect(employee.getRole()).toBe("Employee");
+// });
+
+// test if employee class returns an object
+test("Can invoke employee constructor", () => {
+  const employee = new Employee();
+  expect(typeof employee).toBe("object");
+});
+
+// tests for constructor arguments
+test("Test name parameter", () => {
+  const name = "Leticia";
+  const employee = new Employee(name);
+  expect(employee.name).toBe(name);
+});
+
+test("Test id parameter", () => {
+  const id = 1234;
+  const employee = new Employee("Leticia", id);
+  expect(employee.id).toBe(id);
+});
+
+test("Test email parameter", () => {
+  const email = "leticialnay@gmail.com";
+  const employee = new Employee("Leticia", 1234, email);
+  expect(employee.email).toBe(email);
+});
+
+// tests for methods
+test("Test name method", () => {
+  const name = "Leticia";
+  const e = new Employee(name);
+  expect(e.getName()).toBe(name);
+});
+
+test("Test id method", () => {
+  const id = 1234;
+  const e = new Employee("Leticia", id);
+  expect(e.getId()).toBe(id);
+});
+
+test("Test email method", () => {
+  const email = "leticialnay@gmail.com";
+  const e = new Employee("Leticia", 1234, email);
+  expect(e.getEmail()).toBe(email);
+});
+
+test("Get role should return employee", () => {
+  const value = "Employee";
+  const e = new Employee("Leticia", 1234, "leticialnay@gmail.com");
+  expect(e.getRole()).toBe(value);
 });
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const generateHtml = require("./src/generateHtml");
+// const generateHtml = require("./lib/generateHtml");
 const fs = require("fs");
 const employeeArr = [];
 
@@ -128,4 +128,105 @@ const addEmployee = () => {
       }
     });
 };
+
+function createHtml() {
+  const html = [];
+
+  function mngrHtml(employee) {
+    return `    
+		<!-- Manager -->
+		<div class="col s4 m4 l4 .center-align grey lighten-2">
+		<div class="card center-align">
+		<div class="blue darken-2 white-text">
+	   <div> <h5>${employee.name}</h5></div>
+	   <div><i class="material-icons">local_cafe</i><h6>Manager</h6></div>
+	   </div>
+	   <div>Employee ID: ${employee.id}
+	   <div>Email: <a href="mailto:${employee.email}">${employee.email}</a></div>
+	   <div>Office number: ${employee.officeNumber}</div>
+	  
+	   </div>
+	</div>
+	</div>	
+		`;
+  }
+
+  function engrHtml(employee) {
+    return `<!-- Engineer -->
+		
+			<div class="col s4 m4 l4 .center-align grey lighten-2">
+				<div class="card center-align">
+				<div class="blue darken-2 white-text">
+			   <div> <h5>${employee.name}</h5></div>
+			   <div><i class="material-icons">computer</i><h6>Engineer</h6></div>
+			   </div>
+			   <div>Employee ID: ${employee.id}
+			   <div>Email: <a href="mailto:${employee.email}">${employee.email}</a></div>
+			   <div>Github: <a href="https://github.com/${employee.github}">${employee.github}</a></div>
+			   </div>
+			</div>
+			</div>
+		
+		`;
+  }
+
+  function intHtml(employee) {
+    return `<!-- Intern -->
+		<div class="col s4 m4 l4 .center-align grey lighten-2">
+		<div class="card center-align">
+		<div class="blue darken-2 white-text">
+	   <div> <h5>${employee.name}</h5></div>
+	   <div><i class="material-icons">school</i><h6>Intern</h6></div>
+	   </div>
+	   <div>Employee ID: ${employee.id}
+	   <div>Email: <a href="mailto:${employee.email}">${employee.email}</a></div>
+	   <div>School: ${employee.school}</div>
+	   </div>
+	</div>
+	</div>`;
+  }
+
+  for (i = 0; i < employeeArr.length; i++) {
+    let teamMember = employeeArr[i];
+    // console.log(teamMember.getRole());
+    if (teamMember.getRole() === "Manager") {
+      html.push(mngrHtml(teamMember));
+    } else if (teamMember.getRole() === "Engineer") {
+      html.push(engrHtml(teamMember));
+    } else {
+      html.push(intHtml(teamMember));
+    }
+  }
+  return html.join("");
+}
+
+function starterHtml() {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Team Profile</title>
+</head>
+<body>
+    <div class=".center-align red lighten-4"><h2 style="text-align: center;" class="red lighten-4">Team Profile  Generator</h2></div>
+	
+			<div class="row">
+				<!-- START OF TEAM MEMBER CARDS-->
+	
+				${createHtml()}
+				
+				<!-- END OF TEAM MEMBER CARDS-->
+		
+	</body>
+	</html>`;
+}
+
 initializeQuestions();
